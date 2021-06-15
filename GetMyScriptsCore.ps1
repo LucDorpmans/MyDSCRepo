1ad4b0a7fed40b5c4dd6422781a4fcc6db12b4d4
LucDorpmans <GitHub@sure.nl>
2021-06-15 19:30:11 +0200

    Delete files

--- GetMyScriptsCore.ps1
+++
@@ -1,9 +0,0 @@
-# GetMyScriptsCore.ps1
-Function Get-MyScript { Param( [string]$AFile,[switch]$EditFile = $False, 
-							   [string]$SPath = "$env:USERPROFILE\Downloads\")
-			Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/TestRepo/main/$AFile"  -Outfile "$SPath$AFile" 
-			If ($EditFile) { Notepad  ("$SPath$AFile" )} }
-		
-Get-MyScript "PowerShell-Core-Download+Install.ps1" 
-
-
