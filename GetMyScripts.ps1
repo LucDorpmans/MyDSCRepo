872d9c27419c70565302981e7309117d4e4066d4
LucDorpmans <GitHub@sure.nl>
2021-06-15 19:16:31 +0200

    Update GetMyScripts.ps1

--- GetMyScripts.ps1
+++ GetMyScripts.ps1
@@ -1,7 +1,7 @@
 # GetMyScripts.ps1
 Function Get-MyScript { Param( [string]$AFile,[switch]$EditFile = $False, 
 							   [string]$SPath = "$env:USERPROFILE\Downloads\")
-			Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/TestRepo/main/$AFile"  -Outfile "$SPath$AFile" 
+			Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/MyDSCRepo/main/$AFile"  -Outfile "$SPath$AFile" 
 			If ($EditFile) { PSEdit  ("$SPath$AFile" )} }
 		
 Get-MyScript "Chrome-Download+Run-Installer.ps1" 
