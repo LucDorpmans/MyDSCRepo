426825b904c3270f7b9aa9f8e97352084973db4c
LucDorpmans <GitHub@sure.nl>
2021-06-15 14:10:08 +0200

    Add 55202 Lab files

---
+++ 55202ACLabFiles/Get-DSC-Eventlogs.ps1
@@ -0,0 +1,5 @@
+﻿Get-WinEvent -LogName "Microsoft-Windows-Dsc/Operational"
+Get-WinEvent -LogName "Microsoft-Windows-Dsc/Analytic"
+Get-WinEvent -LogName "Microsoft-Windows-DSC/Analytic" -Oldest 
+
+wevtutil.exe set-log "microsoft-windows-dsc/analytic" /quiet:true /enabled:true
