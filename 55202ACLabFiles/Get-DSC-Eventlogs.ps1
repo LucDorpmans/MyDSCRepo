81f20d879552c989c2e06669b99c60b4ef31fd62
LucDorpmans <GitHub@sure.nl>
2021-06-15 15:28:37 +0200

    Update Get-DSC-Eventlogs.ps1

--- 55202ACLabFiles/Get-DSC-Eventlogs.ps1
+++ 55202ACLabFiles/Get-DSC-Eventlogs.ps1
@@ -1,5 +1,8 @@
-﻿Get-WinEvent -LogName "Microsoft-Windows-Dsc/Operational"
-Get-WinEvent -LogName "Microsoft-Windows-Dsc/Analytic"
+﻿# Enable Analytic EventLogs
+wevtutil.exe set-log "microsoft-windows-dsc/analytic" /quiet:true /enabled:true
+
+# List DSC EventLogs
+Get-WinEvent -LogName "Microsoft-Windows-Dsc/Operational"
+Get-WinEvent -LogName "Microsoft-Windows-Dsc/Analytic" # Produces Error
 Get-WinEvent -LogName "Microsoft-Windows-DSC/Analytic" -Oldest 
 
-wevtutil.exe set-log "microsoft-windows-dsc/analytic" /quiet:true /enabled:true
