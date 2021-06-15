659de034362d76b179bee75be6d7a4b0c548cda4
LucDorpmans <GitHub@sure.nl>
2021-06-15 19:11:23 +0200

    Upd

---
+++ Get55202LabFiles.ps1
@@ -0,0 +1,50 @@
+# Get55202LabFiles.ps1
+Function Get-MyScript { Param( [string]$AFile,[switch]$EditFile = $False, 
+							   [string]$SPath = "$env:USERPROFILE\Downloads\")
+			Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/MyDSPRepo/main/$AFile"  -Outfile "$SPath$AFile" 
+			If ($EditFile) { PSEdit ("$SPath$AFile" )} }
+
+
+Function DownloadLabFiles {
+Param(
+    [string]$Owner,
+    [string]$Repository,
+    [string]$Path,
+    [string]$DestinationPath
+    )
+
+    $baseUri = "https://api.github.com/"
+    $args = "repos/$Owner/$Repository/contents/$Path"
+    $wr = Invoke-WebRequest -Uri $($baseuri+$args)
+    $objects = $wr.Content | ConvertFrom-Json
+    $files = $objects | where {$_.type -eq "file"} | Select -exp download_url
+    $directories = $objects | where {$_.type -eq "dir"}
+    
+    $directories | ForEach-Object { 
+        DownloadFilesFromRepo -Owner $Owner -Repository $Repository -Path $_.path -DestinationPath $($DestinationPath+$_.name)
+    }
+    
+    if (-not (Test-Path $DestinationPath)) {
+        # Destination path does not exist, let's create it
+        try {
+            New-Item -Path $DestinationPath -ItemType Directory -ErrorAction Stop
+        } catch {
+            throw "Could not create path '$DestinationPath'!"
+        }
+    }
+
+    foreach ($file in $files) {
+        $fileDestination = Join-Path $DestinationPath (Split-Path $file -Leaf)
+        try {
+            Invoke-WebRequest -Uri $file -OutFile $fileDestination -ErrorAction Stop -Verbose
+            "Grabbed '$($file)' to '$fileDestination'"
+        } catch {
+            throw "Unable to download '$($file.path)'"
+        }
+    }
+
+}
+
+
+DownloadLabFiles -Owner "LucDorpmans" -Repository My-WS-013-Repo -Path 55202ACLabFiles -DestinationPath C:\Library\Github\
+
