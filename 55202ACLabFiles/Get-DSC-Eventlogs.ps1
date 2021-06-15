a6c73d73b6b121671bb7c0e1182480c59b6ea3ea
LucDorpmans <GitHub@sure.nl>
2021-06-15 15:56:09 +0200

    Update Get-DSC-Eventlogs.ps1

--- 55202ACLabFiles/Get-DSC-Eventlogs.ps1
+++ 55202ACLabFiles/Get-DSC-Eventlogs.ps1
@@ -5,10 +5,10 @@ wevtutil.exe set-log "microsoft-windows-dsc/analytic" /quiet:true /enabled:true
 
 #PowerShell:
 # Enable-PSTrace [-Force] [-AnalyticOnly] [<CommonParameters>]
-Enable-PSTrace 
+Enable-PSTrace -Force
 
 # List DSC EventLogs
 Get-WinEvent -LogName "Microsoft-Windows-Dsc/Operational"
 Get-WinEvent -LogName "Microsoft-Windows-Dsc/Analytic" # Produces Error
-Get-WinEvent -LogName "Microsoft-Windows-DSC/Analytic" -Oldest 
+Get-WinEvent -LogName "Microsoft-Windows-DSC/Analytic" -Oldest -MaxEvents 15
 
