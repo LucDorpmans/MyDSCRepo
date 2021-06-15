c9a87bdefa9e03722e7cfcbca1bd18cbd14163c6
LucDorpmans <GitHub@sure.nl>
2021-06-15 15:21:36 +0200

    Add SleepDemo

---
+++ 55202ACLabFiles/Lab Files/Module 6 DSC Pull and Push Servers/ScriptDemoSleep1.ps1
@@ -0,0 +1,31 @@
+﻿# Demo the script sleeping
+Configuration ScriptDemoShowScript {
+
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+
+Node Studentserver1 {
+    Script ScriptDemoShowScript {
+        GetScript = {
+            @{
+                GetScript = $GetScript
+                SetScript = $SetScript
+                TestScript = $TestScript
+                Result = "TestScript"
+                }
+           }
+        SetScript = {
+            Write-Verbose "Sleeping for 15 seconds"
+            Start-Sleep 15
+            Write-Verbose "Script Task Complete"
+            }
+        TestScript = { 
+            $false 
+            }
+        }
+    }
+}
+
+
+ScriptDemoShowScript -OutputPath "C:\Demo\ScriptDemoShowScript"
+
+Start-DscConfiguration -Path "C:\Demo\ScriptDemoShowScript" -Verbose -Wait
