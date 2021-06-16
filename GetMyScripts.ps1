daf3682ba7134901d6577dd8a36c27ca3140985a
LucDorpmans <GitHub@sure.nl>
2021-06-16 18:04:35 +0200

    Upd

--- GetMyScripts.ps1
+++ GetMyScripts.ps1
@@ -7,5 +7,5 @@ Function Get-MyScript { Param( [string]$AFile,[switch]$EditFile = $False,
 Get-MyScript "EdgeMSI-Download-Only-Complete.ps1" -EditFile
 Get-MyScript "Edge-InstallOnly.ps1" -EditFile
 Get-MyScript "PowerShell-Core-Download+Install.ps1"
-Get-MyScript "Get55202-Files.ps1" -EditFile
+Get-MyScript "Get55202A-Files.ps1" -EditFile
 Get-MyScript "Chrome-Download+Run-Installer.ps1" 
