31bf9118cf99bc4a827d65d4b34b14392d165ae0
LucDorpmans <GitHub@sure.nl>
2021-06-16 17:40:53 +0200

    Update Get55202-Files.ps1

--- Get55202-Files.ps1
+++ Get55202-Files.ps1
@@ -40,5 +40,5 @@ Param(
 }
 
 
-DownloadLabFiles -Owner "LucDorpmans" -Repository MyDSCRepo -Path 55202ACLabFiles -DestinationPath C:\55202-Files\
+DownloadLabFiles -Owner "LucDorpmans" -Repository MyDSCRepo -Path 55202-Files -DestinationPath C:\55202-Files\
 
