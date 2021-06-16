32dcb1a96ec409a5197622187956b97085a66dd7
LucDorpmans <GitHub@sure.nl>
2021-06-16 17:33:03 +0200

    Update GetMyScripts.ps1

--- GetMyScripts.ps1
+++ GetMyScripts.ps1
@@ -4,8 +4,8 @@ Function Get-MyScript { Param( [string]$AFile,[switch]$EditFile = $False,
 			Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/MyDSCRepo/main/$AFile"  -Outfile "$SPath$AFile" 
 			If ($EditFile) { PSEdit  ("$SPath$AFile" )} }
 		
-Get-MyScript "Chrome-Download+Run-Installer.ps1" 
 Get-MyScript "EdgeMSI-Download-Only-Complete.ps1" -EditFile
 Get-MyScript "Edge-InstallOnly.ps1" -EditFile
-Get-MyScript "PowerShell-Core-Download+Install.ps1" -EditFile
-Get-MyScript "Get55202LabFiles.ps1" -EditFile
+Get-MyScript "PowerShell-Core-Download+Install.ps1"
+Get-MyScript "Get55202-Files.ps1" -EditFile
+Get-MyScript "Chrome-Download+Run-Installer.ps1" 
